ce35147730f42550a85e5664548a0f78b947ff91
tianaFomicheva <tianirika@gmail.com>
2020-10-27 00:21:33 +0500

    цепочка асинхронных вызовов

--- watcher.js
+++ watcher.js
@@ -9,122 +9,148 @@ let prevstate, newstate
 
 
 
-export default function(onstart = false){
+export default function(onstart = false) {
 
-    prevstate = !onstart ? getState(directory) : getArray(getLastState());
+// prevstate  = async function(onstart) {
+//     return  !onstart ? getState(directory) : async (req, res) => {
+//         try {
+//     await getLastState()
+//         }
+//         catch{}
+//     }
+//
+//
+//
+// }
 
-async function getLastState() {
+prevstate =     async function getLastState() {
 
+try {
+    const result = await db.query('SELECT state FROM dirtree_states ORDER BY id  DESC LIMIT 1')
 
-        await db.query('SELECT state FROM dirtree_states ORDER BY id  DESC LIMIT 1')
-            .then(res => {
-                let row = res[0]
-                return row.state;
-            })
+    await new Promise((res,rej) => res(JSON.parse(result[0]["state"]))).then(data => parsing(data))
+    // const state_res = await parsing(parse_res);
+        .then(data=>{
+            newstate = getState(directory);
+            let added = diff(newstate, data);
+            let removed = diff(data, newstate);
+            if (added.length > 0 || removed.length > 0) {
+                console.log(333)
 
-            .catch(error => {
+                let action = ((added.length > 0) ? ' Добавлен -' + added.join(' Добавлен -') : '') + ((removed.length > 0) ? 'Удален - ' + removed.join(' Удален - ') : '');
+            insertState(newstate, action)
+            }
+            console.log(added, removed);
+
+        })
+    // return parse_res
+
+}
+            catch(error){
                 console.log('ERROR:', error);
-            });
-}
+            }
+    }
+prevstate()
 
+    function parsing(parse, state = []) {
+        Object.keys(parse).forEach(function (key) {
+            if (typeof (parse[key]) == 'string' && key == 'path') {
+                state.push(parse[key])
+            } else if (key == 'children') {
+                Object.keys(parse[key]).forEach(function (key1) {
+                    parsing(parse[key][key1], state);
+                });
+            }
 
-function getArray(json_tree){
-    let state = [];
-    let parse = JSON.parse(json_tree)
+        })
 
-    Object.keys(parse).forEach(function(key){
-        if(key == 'children') {
-            Object.values(parse[key]).forEach(function (value) {
-                // if (state.indexOf(child1) == -1) {
-                //     // state.push(child1.path);
-                // }
-                state.push(value.path)
-            });
-        }
+        return state;
 
-    })
-    return state;
-}
-
-    let watcher = chokidar.watch(directory, {
-        persistent: true,
-        alwaysState: true
-    })
-
-    function extract(x, arr = []) {
-        if (typeof (x) == 'object') {
-            Object.keys(x).forEach(function (key) {
-                extract(x[key], arr);
-            });
-        } else {
-            arr.push(x);
-        }
-        return arr
     }
 
 
-    function getState(dirname) {
-        let state = [];
 
-        dirtree(dirname, {normalizePath: false}, (item) => {
-            delete item.size;
-            delete item.extension;
-            delete item.type;
-            delete item.name;
-            if (item.children) {
-                let point = extract(item.children);
-                point.forEach(function (child) {
-                    if (state.indexOf(child) == -1) {
-                        state.push(child);
-                    }
+
+        let watcher = chokidar.watch(directory, {
+            persistent: true,
+            alwaysState: true
+        })
+
+        function extract(x, arr = []) {
+            if (typeof (x) == 'object') {
+                Object.keys(x).forEach(function (key) {
+                    extract(x[key], arr);
                 });
             } else {
-                state.push(item.path);
+                arr.push(x);
             }
-        });
-        return state;
-    }
-
-
-
-
-
-    watcher.on('all', () => {
-        newstate = getState(directory);
-
-
-
-        let added = diff(newstate, prevstate);
-
-
-        let removed = diff(prevstate, newstate);
-
-        if (added.length > 0 || removed.length > 0) {
-            let action = ((added.length > 0) ? ' Добавлен -' + added.join(' Добавлен -') : '') + ((removed.length > 0) ? 'Удален - ' + removed.join(' Удален - ') : '');
-
-            async function insertState() {
-                try {
-                    await db.query('INSERT INTO dirtree_states(state, action) VALUES($1, $2) RETURNING id', [JSON.stringify(dirtree(directory)).trim(), action])
-                        .then(res => {
-                            let row = res[0]
-                            console.log('Создана запись, id = ' + row.id);
-                        })
-
-                        .catch(error => {
-                            console.log('ERROR:', error);
-                        });
-
-                } catch {
-                    console.log('error while insert')
-                }
-            }
-
-            insertState().then(() => console.log('done'));
-
+            return arr
         }
-        prevstate = newstate;
-    });
+
+
+        function getState(dirname) {
+            let state = [];
+
+            dirtree(dirname, {normalizePath: false}, (item) => {
+                // delete item.size;
+                // delete item.extension;
+                // delete item.type;
+                // delete item.name;
+                if (item.children) {
+                    let point = extract(item.children);
+                    point.forEach(function (child) {
+                        if (state.indexOf(child) == -1) {
+                            state.push(child);
+                        }
+                    });
+                } else if(item.type !== 'directory'){
+                    state.push(item.path);
+                }
+            });
+            return state;
+        }
+
+
+        watcher.on('all', () => {
+            newstate = getState(directory);
+
+
+            // let added = diff(newstate, prevstate);
+
+
+            // let removed = diff(prevstate, newstate);
+
+            // if (added.length > 0 || removed.length > 0) {
+            //     let action = ((added.length > 0) ? ' Добавлен -' + added.join(' Добавлен -') : '') + ((removed.length > 0) ? 'Удален - ' + removed.join(' Удален - ') : '');
+            //
+
+            prevstate = newstate;
+        });
+        async function insertState(state, action) {
+            console.log(state, action);
+           return false;
+            try {
+                await db.query('INSERT INTO dirtree_states(state, action) VALUES($1, $2) RETURNING id', [JSON.stringify(dirtree(directory)).trim(), action])
+                    .then(res => {
+                        let row = res[0]
+                        console.log('Создана запись, id = ' + row.id);
+                    })
+
+                    .catch(error => {
+                        console.log('ERROR:', error);
+                    });
+
+            } catch {
+                console.log('error while insert')
+            }
+        }
+
+        // insertState().then(() => console.log('done'));
     function diff(a, b) {
         return a.filter((e) => !~b.indexOf(e));
     }
 }
+
+
+
+
